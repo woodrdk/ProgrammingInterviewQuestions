d1c1007add79a27582fa866bd821cd01793e772f
Robert Wood Jr <rdrwood@gmail.com>
2019-12-16 22:17:05 -0800

    setup array for question 2

--- src/com/rdkwood/Main.java
+++ src/com/rdkwood/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         // Questions Being -- In an array 1-100 numbers are stored, one number is missing how do you find it?
         // Makes a test array
-        int numArray[] = new int[100];
+        int[] numArray = new int[100];
         for(int i = 1; i <= 100; i++){
             if(i != 47){
                 numArray[i-1] = i;
@@ -19,8 +19,25 @@ public class Main {
         for(int j = 1; j <= 100; j++){
             // Checks if the array is missing a number and tells user what it is
             if(numArray[j-1] != j){
-                System.out.println("Missing number " + j);
+                System.out.println("Missing number in an array of 1-100 is " + j);
             }
+        }
+
+        // -----------------------------------------------------------------------------------------------------
+        // Question Being --In an array 1-100 exactly one number is duplicate how do you find it?
+        // Makes new test array
+        int[] numArray2 = new int[100];
+        for(int z = 0; z < 100; z++){
+            if(z == 47){
+                numArray2[z] = z;
+            }
+            else{
+                numArray2[z] = z+1;
+            }
+        }
+        for(int y = 0; y < 99; y++){
+            System.out.println(numArray2[y] + " Y");
+
         }
     }
 }
