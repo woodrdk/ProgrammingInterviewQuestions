c3c1c04402d41dc7f6df0a0a5cf46afa026d7fd9
Robert Wood Jr <rdrwood@gmail.com>
2019-12-16 21:33:39 -0800

    comments

--- src/com/rdkwood/Main.java
+++ src/com/rdkwood/Main.java
@@ -6,14 +6,18 @@ public class Main {
 	    // Test code if functioning
         // System.out.println("hello world");
 
-        // In an array 1-100 numbers are stored, one number is missing how do you find it?
+        // Questions Being -- In an array 1-100 numbers are stored, one number is missing how do you find it?
+        // Makes a test array
         int numArray[] = new int[100];
         for(int i = 1; i <= 100; i++){
             if(i != 47){
                 numArray[i-1] = i;
             }
         }
+
+        // Run a loop of numbers 1-100
         for(int j = 1; j <= 100; j++){
+            // Checks if the array is missing a number and tells user what it is
             if(numArray[j-1] != j){
                 System.out.println("Missing number " + j);
             }
