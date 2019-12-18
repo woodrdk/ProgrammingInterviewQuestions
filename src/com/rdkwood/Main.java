11a7540384ff22dbafc1d48f6e98fdb85f36809b
Robert Wood Jr <rdrwood@gmail.com>
2019-12-17 23:08:46 -0800

    comments for questions

--- src/com/rdkwood/Main.java
+++ src/com/rdkwood/Main.java
@@ -6,6 +6,9 @@ public class Main {
 	    // Test code if functioning
         // System.out.println("hello world");
 
+
+        // Array Questions
+
         // Questions Being -- In an array 1-100 numbers are stored, one number is missing how do you find it?
         // Makes a test array
         int[] numArray = new int[100];
@@ -41,5 +44,25 @@ public class Main {
             // System.out.println(numArray2[y] + " Y");
 
         }
+
+
+
+        // In an array 1-100 multiple numbers are duplicates, how do you find it? (solution)
+        // One trick in this programming questions is by using HashMap or Hashtable, we can store a number as key
+        // and its occurrence as value if the number is already present in Hashtable then increment its value or
+        // insert value as 1 and later on print all those numbers whose values are more than one.
+
+        // Given two arrays, 1,2,3,4,5 and 2,3,1,0,5 find which number is not present in the second array.
+        // Here is a quick tip to solve this programming question: put the elements of the second array in the
+        // Hashtable and for every element of the first array, check whether it’s present in the hash or not, O/P
+        // all those elements from the first array that are not present in the hash table
+
+        // How do you find the second highest number in an integer array?
+
+        // How to find all pairs in an array of integers whose sum is equal to the given number? (solution)
+
+        // How to find the largest and smallest number in an array? (solution)
+
+        // How to find the top two maximum number in an array? (solution)
     }
 }
