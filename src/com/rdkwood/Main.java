ba5565924d4bc22b8ce78d19ccd625ebf814c0d8
Robert Wood Jr <rdrwood@gmail.com>
2019-12-18 23:03:22 -0800

    refactoring and set up of question 3

--- src/com/rdkwood/Main.java
+++ src/com/rdkwood/Main.java
@@ -8,54 +8,14 @@ public class Main {
 
 
         // Array Questions
-
-        // Questions Being -- In an array 1-100 numbers are stored, one number is missing how do you find it?
-        // Makes a test array
-        int[] numArray = new int[100];
-        for(int i = 1; i <= 100; i++){
-            if(i != 47){
-                numArray[i-1] = i;
-            }
-        }
-
-        // Run a loop of numbers 1-100
-        for(int j = 1; j <= 100; j++){
-            // Checks if the array is missing a number and tells user what it is
-            if(numArray[j-1] != j){
-                System.out.println("Missing number in an array of 1-100 is " + j);
-            }
-        }
-
-        // -----------------------------------------------------------------------------------------------------
-        // Question Being --In an array 1-100 exactly one number is duplicate how do you find it?
-        // Makes new test array
-        int[] numArray2 = new int[100];
-        for(int z = 0; z < 100; z++){
-            if(z == 47){
-                numArray2[z] = z;
-            }
-            else{
-                numArray2[z] = z+1;
-            }
-        }
-        // loop to look for duplicate number within the array
-        for(int y = 0; y < 99; y++){
-            // test code to display the array contents
-            // System.out.println(numArray2[y] + " Y");
-
-        }
-
-
-
-
-
+        arrayOne();
+        arrayTwo();
+        arrayThree();
         // ****************  String Programming Interview Questions ********************
-
         // Write code to check a String is palindrome or not?
            //     A palindrome is those String whose reverse is equal to the original.
         //     This can be done by using either StringBuffer reverse() method or by technique
         //     demonstrated in the solution here.
-
 
         // Write a method which will remove any given character from a String?
           //\ hint: you can remove a given character from String by converting it into a character array and then
@@ -188,5 +148,46 @@ public class Main {
         // Write a program to implement a blocking queue in Java?
         //  Write a program for the producer-consumer problem?
 
+    }
+    public static void arrayOne(){
+        // Questions Being -- In an array 1-100 numbers are stored, one number is missing how do you find it?
+        // Makes a test array
+        int[] numArray = new int[100];
+        for(int i = 1; i <= 100; i++){
+            if(i != 47){
+                numArray[i-1] = i;
+            }
+        }
+
+        // Run a loop of numbers 1-100
+        for(int j = 1; j <= 100; j++){
+            // Checks if the array is missing a number and tells user what it is
+            if(numArray[j-1] != j){
+                System.out.println("Missing number in an array of 1-100 is " + j);
+            }
+        }
+    }
+    public static void arrayTwo(){
+        // Question Being --In an array 1-100 exactly one number is duplicate how do you find it?
+        // Makes new test array
+        int[] numArray2 = new int[100];
+        for(int z = 0; z < 100; z++){
+            if(z == 47){
+                numArray2[z] = z;
+            }
+            else{
+                numArray2[z] = z+1;
+            }
+        }
+        // loop to look for duplicate number within the array
+        for(int y = 0; y < 99; y++){
+            // test code to display the array contents
+            // System.out.println(numArray2[y] + " Y");
+
+        }
+    }
+
+    public static void arrayThree(){
+        // How to find the largest and smallest number in an array?
     }
 }
