17db095dc229cf52e6252683a2d7ba45a9bb5775
Robert Wood Jr <rdrwood@gmail.com>
2019-12-21 17:46:43 -0800

    added number questions class and questions

---
+++ src/com/rdkwood/NumberQuestions.java
@@ -0,0 +1,35 @@
+package com.rdkwood;
+
+public class NumberQuestions {
+
+}
+// ******************* Programming Questions on Numbers ***************************************
+
+// Write code to check whether a no is a power of two or not?
+// Write a program to check whether a number is a palindrome or not?
+// Check out this post which shows how to reverse a number in Java and can be used to find if its palindrome
+// or not.
+
+// Write code to check whether an integer is Armstrong number or not?
+// Here is a Java program to find Armstrong number, you can use the same logic to write code in any other
+// programming language like C and C++.
+
+// Write a program to find all prime number up to a given number?
+// Here is another Java program to find prime numbers and print them.
+// By using logic demonstrated in this program; you can write a similar program in C and C++.
+
+// Write a function to compute Nth Fibonacci number? Both iterative and recursive?
+// You can check this Java program to print Fibonacci Series using recursion and iteration.
+
+// How to check if a number is binary?
+//  For this question, you need to write a function which will accept an integer and return true if it
+//  contains only 0 and 1 e.g. if the input is 123 then your function will return false,
+//  for 101 it should return true.
+
+// How to reverse an integer in Java?
+// How to count a number of set bits in given integer?
+// How to find the sum of digits of a number using recursion?
+// How to swap two numbers without using temp variable?
+// How to find the largest of three integers in Java?
+// Write a program to find prime factors of an integer?
+// How to add two integers without using arithmetic operator?
